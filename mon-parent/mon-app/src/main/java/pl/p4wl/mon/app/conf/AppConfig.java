e9a9bcf9e1b4c15b51f5e93770defb5798058692
p4wl <pawel.e.rosner@gmail.com>
2017-02-11 00:07:08 +0100

    landing page

--- mon-parent/mon-app/src/main/java/pl/p4wl/mon/app/conf/AppConfig.java
+++ mon-parent/mon-app/src/main/java/pl/p4wl/mon/app/conf/AppConfig.java
@@ -1,10 +1,20 @@
 package pl.p4wl.mon.app.conf;
 
+import org.springframework.beans.factory.config.PropertyPlaceholderConfigurer;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.PropertySource;
 import pl.p4wl.mon.web.config.WebConfig;
 
 @Configuration
 @Import(WebConfig.class)
+@PropertySource("classpath:application.properties")
 public class AppConfig {
+
+    @Bean
+    public PropertyPlaceholderConfigurer propertyPlaceholderConfigurer() {
+        return new PropertyPlaceholderConfigurer();
+    }
+
 }
--- mon-parent/mon-web/src/main/java/pl/p4wl/mon/web/controllers/LandingPageController.java
+++ mon-parent/mon-web/src/main/java/pl/p4wl/mon/web/controllers/LandingPageController.java
@@ -1,14 +1,17 @@
 package pl.p4wl.mon.web.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class LandingPageController {
 
-    @GetMapping("/hello")
-    public String hello() {
-        return "";
+    @GetMapping("/")
+    public String hello(@RequestParam("name") String name, Model model) {
+        model.addAttribute("name", name);
+        return "login";
     }
 
 }
